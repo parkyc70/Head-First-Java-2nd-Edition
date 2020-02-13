ca3a21758126f3177fe41ec555f93651d72bdfb1
Yuncheol Park <parkyc70@gmail.com>
2020-02-13 13:15:47 +0900

    Hello world! 출력 추가

--- chap01/BeerSong.java
+++ chap01/BeerSong.java
@@ -22,6 +22,7 @@ public class BeerSong {
             else
             {
                 System.out.println("No more bottles of beer on the wall");
+                System.out.println("Hello world!");
             }
         }
     }
